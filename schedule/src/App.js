b5f15e3d2ac8e39d85c1b81288cb74b97e1e2ef3
Marcus Liu <marcus.liu@stonybrook.edu>
2022-12-24 00:03:18 -0500

    Edit

---
+++ schedule/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './header/Header';
+import Planner from './planner/Planner';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Planner />
+    </div>
+  );
+}
+
+export default App;
