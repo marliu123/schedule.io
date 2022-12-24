a5483bdda3c1ef9dde06fc16eaaa21486e87146c
Marcus Liu <marcus.liu@stonybrook.edu>
2022-12-24 01:13:40 -0500

    edit

--- schedule/src/planner/Planner.jsx
+++ schedule/src/planner/Planner.jsx
@@ -3,11 +3,33 @@ import './planner.css';
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
 const Planner = () => {
+    const currentDate = document.querySelector(".current-date");
+    const daysTag = document.querySelector(".days")
+    let date = new Date();
+    let currYear = date.getFullYear();
+    let currMonth = date.getMonth();
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    const currDate = () =>{
+        let lastDay = new Date(currYear, currMonth + 1, 0).getDate();
+        let liTag = '';
+        for(let i = 1; i <= lastDay; i++){
+            liTag += `<li>${i}</li>`;
+        }
+
+
+        currentDate.innerHTML = `${months[currMonth]} ${currYear}`;
+        daysTag.innerHTML = liTag;
+    }
+
+    currDate();
+    
+
     return(
         <div className='body'>
             <div className='wrapper'>
                 <header>
-                    <p className = 'current-date'> December 2022 </p>
+                    <p className = 'current-date'></p>
                     <div className='icons'>
                         <span>< HiChevronLeft /></span>
                         <span>< HiChevronRight /></span>
@@ -24,37 +46,6 @@ const Planner = () => {
                         <li>Sat</li>
                     </ul>
                     <ul className='days'>
-                        <li>28</li>
-                        <li>29</li>
-                        <li>30</li>
-                        <li>31</li>
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
-                        <li>5</li>
-                        <li>6</li>
-                        <li>7</li>
-                        <li>8</li>
-                        <li>9</li>
-                        <li>10</li>
-                        <li>11</li>
-                        <li>12</li>
-                        <li>13</li>
-                        <li>14</li>
-                        <li>15</li>
-                        <li>16</li>
-                        <li>17</li>
-                        <li>18</li>
-                        <li>19</li>
-                        <li>20</li>
-                        <li>21</li>
-                        <li>22</li>
-                        <li>23</li>
-                        <li>24</li>
-                        <li>25</li>
-                        <li>26</li>
-                        <li>27</li>
                     </ul>
                 </div>
             </div>
